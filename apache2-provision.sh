3a10a5c1d96e73bbb8886a533a16986cc7b36533
David Lopez <dlopmal873@ieszaidinvergeles.org>
2024-11-27 14:30:47 +0100

    primer commmit

---
+++ apache2-provision.sh
@@ -0,0 +1,28 @@
+#
+# Copiar ficheros de configuración de apache2 que incluyen:
+#
+# - apache2.conf,
+# - apolo.olimpo.test.conf
+# - atenea.olimpo.test.conf
+#
+
+cp -v /vagrant/apache2.conf /etc/apache2/
+cp -v /vagrant/apolo.olimpo.test.conf /etc/apache2/sites-available/
+cp -v /vagrant/atenea.olimpo.test.conf /etc/apache2/sites-available/
+
+#
+# Habilitar los servidores virtuales
+
+a2dissite 000-default.conf
+
+a2ensite apolo.olimpo.test.conf
+a2ensite atenea.olimpo.test.conf
+
+#
+# Reiniciar el servicio
+#
+
+# tu código aquí
+
+systecmtl restart apache2
+
